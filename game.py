65e823164463bf58099cec7548a1972284915b02
DaMaTu <matthias.jarvet@gmail.com>
2024-04-22 12:34:06 +0300

    Displaying words

---
+++ game.py
@@ -0,0 +1,76 @@
+
+
+
+HANGMAN_PICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+
+def get_random_word(filename="words.txt"):
+    with open(filename, 'r') as f:
+        words = f.read().splitlines()
+    return random.choice(words)
+
+def display_board(HANGMAN_PICS, missed_letters, correct_letters, secret_word):
+    print(HANGMAN_PICS[len(missed_letters)])
+    print()
+
+    print('Missed letters:', end=' ')
+    for letter in missed_letters:
+        print(letter, end=' ')
+    print()
+
+    blanks = '_' * len(secret_word)
+
+    for i in range(len(secret_word)):
+        if secret_word[i] in correct_letters:
+            blanks = blanks[:i] + secret_word[i] + blanks[i+1:]
+
+    print(' '.join(blanks))
